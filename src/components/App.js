bbff9d0078086e7e66f1ff5cd2b3c9a0ad57d44c
deandev <iamdekanos@gmail.com>
2022-01-08 14:42:42 +0200

    Added ability to create and delete tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
+let globalID = 0;
+
 function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function createTodo(e) {
+    e.preventDefault();
+    setTodos((oldTodos) => {
+      setTask("");
+      return [...oldTodos, { todo: task, id: globalID++ }];
+    });
+  }
+
+  function deleteItem(itemId) {
+    setTodos((oldTodos) => oldTodos.filter((item) => item.id !== itemId));
+  }
+
   return (
-    <div className="App">
+    <div>
       <h1>React todo app for refreshing the memory</h1>
+      <form onSubmit={createTodo}>
+        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+        <button type="submit">Create Todo</button>
+      </form>
+
+      <ul>
+        {todos.map((item, index) => {
+          return (
+            <div key={item.id}>
+              <li>
+                {item.todo} ({item.id})
+              </li>
+              <button onClick={() => deleteItem(item.id)}>delete</button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
